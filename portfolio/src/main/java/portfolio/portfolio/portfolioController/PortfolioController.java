769f03a760a40045885d12da2785250c7d7ba5bd
HariThrive <hramgain@gmail.com>
2025-08-29 15:19:12 +0530

    added the html portfolio

---
+++ portfolio/src/main/java/portfolio/portfolio/portfolioController/PortfolioController.java
@@ -0,0 +1,13 @@
+package portfolio.portfolio.portfolioController;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PortfolioController {
+
+	@GetMapping("/portfolio")
+	public String getPortfolio() {
+		return "portfolio";
+	}
+}
